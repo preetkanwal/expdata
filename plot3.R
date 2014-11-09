4165ca3ba7137598bf60f561a976b81af3d8c0ca
Preet Kanwal <s.preetkanwal@gmail.com>
2014-11-10 03:45:14 +0530

    Updated Plot3.R and plot3.png

--- plot3.R
+++ plot3.R
@@ -22,7 +22,7 @@ lines(epc$Time, epc$Sub_metering_3,
       type = "l")
 
 legend("topright", col = c("black", "red", "blue"),
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, cex = .5)
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, cex = .7)
 
 dev.copy(png, file = "plot3.png", width = 480, height = 480)
 dev.off()
