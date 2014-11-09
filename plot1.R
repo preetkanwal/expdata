ad3cfc54a15f31592d248dd1ec539b51b093fc4c
Preet Kanwal <s.preetkanwal@gmail.com>
2014-11-10 00:50:47 +0530

    Initial Commit containing plot1.R & plot1.png

---
+++ plot1.R
@@ -0,0 +1,17 @@
+setwd("./rwd/expdata/cp1")
+# creating dataframe epc that contains 2075259 obs. of 9 variables
+epc <- read.table("household_power_consumption.txt", sep = ";", header = TRUE,colClasses = c("character", "character", rep("numeric", 7)), na.strings = "?")
+# converting date & time variables to Date/Time class
+epc$Time <- strptime(paste(epc$Date, epc$Time), "%d/%m/%Y %H:%M:%S")
+epc$Date <- as.Date(epc$Date, "%d/%m/%Y")
+#taking subset of epc for dates  2007-02-01 and 2007-02-02
+epc <- subset(epc, epc$Date >= "2007-2-1" & epc$Date <= "2007-2-2")
+
+hist(epc$Global_active_power,
+     col = "red",
+     main = "Global Active Power",
+     xlab = "Global Active Power (Kilowatts)",
+     ylab = "Frequency")
+
+dev.copy(png, file = "plot1.png", width = 480, height = 480)
+dev.off()
